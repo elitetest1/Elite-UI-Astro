7f74b67720783d028e304443ea1f5e9e57a3c8e1
elitetest1 <tizzyprove@gmail.com>
2026-01-31 03:56:09 -0300

    welp

--- astro/misc/_misc_patches.sh
+++ astro/misc/_misc_patches.sh
@@ -24,14 +24,7 @@ BPROP "vendor" "ro.adb.secure" "0"
 
 ROM_BUILD_ID="$(GET_PROP "system" "ro.build.display.id")"
 
-
-if [[ "$ROM_BUILD_ID" == *Astro* ]]; then
-    ASTROROM_PROP="$ROM_BUILD_ID"
-else
-    ASTROROM_PROP="AstroROM ${ROM_VERSION} [${ROM_BUILD_ID}]"
-fi
-
-BPROP "system" "ro.build.display.id" "$ASTROROM_PROP"					  
+BPROP "system" "ro.build.display.id" "EliteUI-Testing-fork"					  
 					
 
 # Remove samsung data gather
--- objectives/r9s/vendor-patches/patches.sh
+++ objectives/r9s/vendor-patches/patches.sh
@@ -2,4 +2,5 @@
 #We are building using latest firmware (latest binary and one ui 8)
 # Latest unlockeable BL is based on binary I (one ui 7) so, we can use OneUI 8 vendor with vendor/tee folder from OneUI 7 (binary I)
 # Requires one ui 7 libsec-ril.so to have working ril
+# anashe
 REMOVE "vendor" "tee"
